76b76044ca1973849c500b929467e429f63807c3
stve <steve.agalloco@gmail.com>
2017-01-13 09:09:59 -0500

    fix itunes command

--- itunes.go
+++ itunes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/everdev/mack"
@@ -17,12 +18,19 @@ var itunes = &cli.Command{
 	Desc: "Add mp3s to iTunes",
 	Argv: func() interface{} { return new(itunesT) },
 	Fn: func(ctx *cli.Context) error {
+		pwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
 		files, _ := filepath.Glob("*.mp3")
 		for i := 0; i < len(files); i++ {
-			fmt.Println("Adding " + files[i])
-			err := mack.Tell("iTunes", "add (POSIX file \""+files[i]+"\"")
+			command := fmt.Sprintf("add (POSIX file \"%s\")", filepath.Join(pwd, files[i]))
+			fmt.Println("Adding " + command)
+			err := mack.Tell("iTunes", command)
 			if err != nil {
-				fmt.Println("Error adding " + files[i])
+				fmt.Println("Error adding "+files[i], err)
 			}
 		}
 
