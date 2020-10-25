667e7a5befc5ac7f6d01800e603df96efb9983e2
Steve Agalloco <steve.agalloco@gmail.com>
2020-10-24 21:40:07 -0400

    Add format command

---
+++ tags/format.go
@@ -0,0 +1,96 @@
+package tags
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+const dir = "."
+
+var fileFormats = [2]string{".wav", ".aif"}
+
+func filesOfType(ext string, overwriteExisting bool) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	var filesToConvert []string
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		if !strings.Contains(file.Name(), ext) {
+			continue
+		}
+
+		filePath := path.Join(dir, file.Name())
+		if _, err := os.Stat(filePath); err != nil {
+			continue
+		}
+
+		// If force is true, we want to convert regardless of if a file exists or not
+		if !overwriteExisting {
+			if _, err := os.Stat(strings.Replace(filePath, ext, ".mp3", 1)); err == nil {
+				fmt.Printf("%s already exists, skipping...\n", file.Name())
+				continue
+			}
+		}
+
+		filesToConvert = append(filesToConvert, filePath)
+	}
+
+	return filesToConvert
+}
+
+func convert(path string, wg *sync.WaitGroup) {
+	extension := filepath.Ext(path)
+	newPath := strings.Replace(path, extension, ".mp3", 1)
+	fmt.Printf("lame --silent -b 320 -h -V2 %s %s\n", path, newPath)
+	cmd := exec.Command("lame", "--silent", "-b 320", "-h", "-V2", path, newPath)
+
+	if output, err := cmd.CombinedOutput(); err != nil {
+		fmt.Println("Build:", err)
+		fmt.Println("Build:", string(output))
+		os.Exit(1)
+	}
+
+	fmt.Printf("Done converting '%s' \n", path)
+	wg.Done()
+}
+
+// Format - a command to format files
+func Format(overwriteExisting bool) {
+	fmt.Printf("overwriteExisting is %v\n", overwriteExisting)
+	var files []string
+
+	for _, format := range fileFormats {
+		fmt.Printf("Detecting %s files...\n", format)
+
+		filesToConvert := filesOfType(format, overwriteExisting)
+		if len(filesToConvert) > 0 {
+			files = append(files, filesToConvert...)
+		}
+	}
+
+	if len(files) == 0 {
+		fmt.Println("No files were found.")
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(files))
+	for _, p := range files {
+		go convert(p, &wg)
+	}
+
+	wg.Wait()
+}
